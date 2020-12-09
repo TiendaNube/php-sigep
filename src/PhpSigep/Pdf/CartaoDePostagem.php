af4e7dd3cee4c106a3624e8cb94252160eae41e1
Everton de Paula <everton@tiendanube.com>
2020-12-09 15:49:39 -0300

    Update src/PhpSigep/Pdf/CartaoDePostagem.php

--- src/PhpSigep/Pdf/CartaoDePostagem.php
+++ src/PhpSigep/Pdf/CartaoDePostagem.php
@@ -730,7 +730,7 @@ class CartaoDePostagem
         $str .= $msg;
         return $str;
     }
-
+    
     public function getEnvioMesmoDestinatario() {
         return $this->envioMesmoDestinatario;
     }
