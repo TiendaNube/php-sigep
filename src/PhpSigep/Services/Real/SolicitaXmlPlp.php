2bb9dc6ee706735bc7ea25fdbe9ea976a9b4864b
Fernando Miguel Caycho Feria <fernando.caycho@globant.com>
2021-10-13 15:31:30 -0300

    resolve unmarshal-plp refactor

--- src/PhpSigep/Services/Real/SolicitaXmlPlp.php
+++ src/PhpSigep/Services/Real/SolicitaXmlPlp.php
@@ -1,13 +1,12 @@
 <?php
+
 namespace PhpSigep\Services\Real;
 
-use PhpSigep\Model\SolicitaXmlPlpResult;
-use PhpSigep\Services\Exception;
-use PhpSigep\Services\Result;
 use PhpSigep\Bootstrap;
+use PhpSigep\Model\SolicitaXmlPlpResult;
 use PhpSigep\Services\Real\Exception\SolicitaXmlPlp\FailedConvertToArrayException;
-use PhpSigep\Services\Real\Exception\SolicitaXmlPlp\FailedConvertXmlException;
 use PhpSigep\Services\Real\Exception\SolicitaXmlPlp\FailedResultException;
+use PhpSigep\Services\Result;
 
 /**
  * @author: Cristiano Soares
@@ -18,15 +17,15 @@ class SolicitaXmlPlp
     /**
      * @param integer $idPlpMaster
      *
-     * @throws \PhpSigep\Services\Exception
      * @return Result<SolicitaXmlPlpResult[]>
+     * @throws \PhpSigep\Services\Exception
      */
     public function execute($idPlpMaster)
     {
         $soapArgs = array(
-            'idPlpMaster'   => $idPlpMaster,
-            'usuario'        => Bootstrap::getConfig()->getAccessData()->getUsuario(),
-            'senha'          => Bootstrap::getConfig()->getAccessData()->getSenha()
+            'idPlpMaster' => $idPlpMaster,
+            'usuario' => Bootstrap::getConfig()->getAccessData()->getUsuario(),
+            'senha' => Bootstrap::getConfig()->getAccessData()->getSenha()
         );
 
         $result = new Result();
@@ -45,19 +44,15 @@ class SolicitaXmlPlp
                 libxml_use_internal_errors(true);
                 $xmlString = iconv('utf-8', 'ISO-8859-1//IGNORE', $r->return);
                 $xml = simplexml_load_string($xmlString, \SimpleXMLElement::class, LIBXML_NOCDATA);
-                if ($xml instanceof \SimpleXMLElement) {
-                    $objectToarray = json_decode(json_encode($xml), true);
-
-                    if ($objectToarray) {
-                        $result->setResult(new SolicitaXmlPlpResult($objectToarray));
-                    } else {
-                        throw new FailedConvertToArrayException('Erro ao converter Object para Array da PLP. Retorno: "' . print_r(json_last_error_msg(), true) . '"');
-                    }
-                } else {
+                if (!($xml instanceof \SimpleXMLElement)) {
                     $xml = simplexml_load_string($r->return);
-                    $objectToarray = json_decode(json_encode($xml), true);
+                }
+                $objectToarray = json_decode(json_encode($xml), true);
+                if ($objectToarray) {
                     $result->setResult(new SolicitaXmlPlpResult($objectToarray));
-               }
+                } else {
+                    throw new FailedConvertToArrayException('Erro ao converter Object para Array da PLP. Retorno: "' . print_r(json_last_error_msg(), true) . '"');
+                }
             } else {
                 throw new FailedResultException('Erro no resultado do XML da PLP. Retorno: "' . print_r($r->return, true) . '"');
             }
